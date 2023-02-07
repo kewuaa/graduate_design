7eca12ae244be2755484f7f744fe151a857f73f8
kewuaa <1692525710@qq.com>
2023-02-07 20:14:39 +0800

    update config.json to config.toml

--- project/config/config.py
+++ project/config/config.py
@@ -1,18 +1,25 @@
+from dataclasses import dataclass
 from pathlib import Path
-import json
-default = {
-    'epoch': 5,
-    'batch_size': 1,
-    'learning_rate': 1e-5,
-    'validation_percent': 0.1,
-}
+
+try:
+    import rtoml as toml
+except ImportError:
+    import tomli as toml
 
 
-def load() -> dict:
-    config_file = Path('./config.json')
-    default_config = default.copy()
+@dataclass(order=False, eq=False)
+class Config:
+    epoch_num: int = 5
+    batch_size: int = 1
+    learning_rate: float = 1e-5
+    validation_percent: float = 0.1
+    weight_decay: float = 1e-8
+
+
+def load() -> Config:
+    config_file = Path('./config.toml')
+    config = {}
     if config_file.exists():
         with open(config_file) as f:
-            config = json.load(f)
-        default_config.update(config)
-    return default_config
+            config = toml.load(f)
+    return Config(**config)
--- train.py
+++ train.py
@@ -1,3 +1,5 @@
+from torch import optim
+from torch import nn
 import torch
 
 from project import data
@@ -12,23 +14,42 @@ min_circle_size = 10
 max_circle_size = 30
 
 
-def train_model(model):
-    pass
+def train_model(model, device, save_checkpoint: bool = True) -> None:
+    config = train_config.load()
+    epoch_num = config['epoch_num']
+    # 1. Create dataset
+
+    # 2. Split into train / validation partitions
+
+    # 3. Create data loaders
+
+    # 4. Set up the optimizer, the loss, the learning rate scheduler
+    # and the loss scaling for AMP
+    # optimizer = optim.RMSprop(
+    #     model.parameters(),
+    #     lr=config['learning_rate'],
+    #     weight_decay=config['weight_decay'],
+    #     momentum=momentum,
+    #     foreach=True
+    # )
+
+    for epoch in range(epoch_num):
+        pass
 
 
 if __name__ == "__main__":
-    logger.info('init data......')
-    data.init(
-        img_num,
-        img_size,
-        max_circle_num,
-        min_circle_size,
-        max_circle_size,
-        180 / projection_num,
-    )
-    logger.info('data successfully init at "./data"')
-    # config = train_config.load()
-    # print(config)
+    # logger.info('init data......')
+    # data.init(
+    #     img_num,
+    #     img_size,
+    #     max_circle_num,
+    #     min_circle_size,
+    #     max_circle_size,
+    #     180 / projection_num,
+    # )
+    # logger.info('data successfully init at "./data"')
+    config = train_config.load()
+    print(config)
     # net = model.Automap(projection_num, img_size)
     # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     # net.to(device)
