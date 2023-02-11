37593841931c105b6211a5069fbe96a0fc442b8d
kewuaa <1692525710@qq.com>
2023-02-11 16:14:16 +0800

    update

--- predict.py
+++ predict.py
@@ -1,50 +1,29 @@
 from pathlib import Path
 
-from torch.utils.data import DataLoader
 from matplotlib import pyplot as plt
 from skimage.transform import iradon
 import numpy as np
+import torch
+import cv2
 
-from project import model
-from project.data import Dataset
+# from project import model
 image_dir = Path('./data/transformed_imgs')
 label_dir = Path('./data/imgs')
 
 
 if __name__ == "__main__":
-    net = model.Automap()
-    net.load('./checkpoints/checkpoint_epoch_1.pth')
-    with Dataset() as dataset:
-        loader = DataLoader(dataset, batch_size=10)
-        for image, label in loader:
-            pre = net(image)
-            break
-
-    image = np.squeeze(image.detach().numpy()[3, ...])
-    pre = np.squeeze(pre.detach().numpy()[3, ...])
-    # label = np.squeeze(label.detach().numpy()[3, ...])
-    label = iradon(image)
-
-    ori_train = image
-    img_result_train = pre
-    reconstruction_fbp_train = label
-
-    # plt.subplot(231), plt.imshow(ori_test, cmap='gray')
-    # plt.title('Original test set'), plt.xticks([]), plt.yticks([])
-
-    # plt.subplot(232), plt.imshow(img_result_test, cmap='gray')
-    # plt.title('Recon'), plt.xticks([]), plt.yticks([])
-
-    # plt.subplot(233), plt.imshow(reconstruction_fbp, cmap='gray')
-    # plt.title('Recon by iradon'), plt.xticks([]), plt.yticks([])
-
-    plt.subplot(234), plt.imshow(ori_train, cmap='gray')
-    plt.title('Original train set'), plt.xticks([]), plt.yticks([])
-
-    plt.subplot(235), plt.imshow(img_result_train, cmap='gray')
-    plt.title('Recon'), plt.xticks([]), plt.yticks([])
-
-    plt.subplot(236), plt.imshow(reconstruction_fbp_train, cmap='gray')
-    plt.title('Recon by iradon'), plt.xticks([]), plt.yticks([])
-
+    # net = model.Automap()
+    # net.load('./checkpoints/checkpoint_epoch_5.pth')
+    i = 3
+    img_path = image_dir / f'{i}.png'
+    label_path = label_dir / f'{i}.png'
+    img = cv2.imread(str(img_path), cv2.IMREAD_GRAYSCALE)
+    label = cv2.imread(str(label_path), cv2.IMREAD_GRAYSCALE)
+    img = cv2.normalize(img, None, -0.5, 0.5, cv2.NORM_MINMAX, cv2.CV_32F)
+    label = cv2.normalize(label, None, -0.5, 0.5, cv2.NORM_MINMAX, cv2.CV_32F)
+    img = torch.Tensor(np.expand_dims(img, axis=0))
+    label = torch.Tensor(np.expand_dims(label, axis=0))
+    # pre = net(img)
+    plt.imshow(np.squeeze(img.detach().numpy()), cmap='gray')
     plt.show()
+    print(img, label, sep='\n')
