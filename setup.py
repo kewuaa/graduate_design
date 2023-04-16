7e48c06ce105c1ee5543e4384f2702a1af580dc8
kewuaa <1692525710@qq.com>
2023-04-16 08:02:36 +0800

    update build system for new extention module

--- setup.py
+++ setup.py
@@ -1,46 +1,60 @@
+from pathlib import Path
 import os
 
 from setuptools import setup, Extension
-opencv_home = os.environ['OPENCV_HOME']
-files = (
-    'src\\graduate_design\\cylib\\circle.c',
-    'src\\graduate_design\\cylib\\transform.cpp'
-)
-suffix = ''
-if not all(os.path.exists(file) for file in files):
-    from Cython.Build import cythonize
-    suffix = 'pyx'
+from setuptools.command.build_ext import build_ext
+
+
+class Build(build_ext):
+    def build_extensions(self):
+        if self.compiler.compiler_type == 'mingw32':
+            for ext in self.extensions:
+                ext.extra_link_args = [
+                    '-static-libgcc',
+                    '-static-libstdc++',
+                    '-Wl,-Bstatic,--whole-archive',
+                    '-lwinpthread',
+                    '-Wl,--no-whole-archive',
+                ]
+        super().build_extensions()
+
+
+
+lib_type = 'static'
+opencv_home = f"{os.environ['OPENCV_HOME']}/{lib_type}"
+opencv_include_dir = opencv_home + '/include'
+opencv_lib_dir = opencv_home + '/x64/mingw/lib'
+library_dirs = [opencv_lib_dir]
+libraries = ['opencv_world470']
+if lib_type == 'static':
+    libraries += list(
+        f.stem[3:]
+        for f in Path(opencv_lib_dir).iterdir()
+        if f.suffix == '.a'
+    )
+suffix = 'pyx'
 
 
 exts = [
     Extension(
-        name='graduate_design.cylib.circle',
+        name='graduate_design.cylib.cylib',
         sources=[
-            f'src\\graduate_design\\cylib\\circle.{suffix or "c"}',
-        ]
-    ),
-    Extension(
-        name='graduate_design.cylib.ctrans',
-        sources=[
-            f'src\\graduate_design\\cylib\\transform.{suffix or "cpp"}',
-            'src\\graduate_design\\cylib\\cpp\\src\\radon_transform.cpp'
+            f'src\\graduate_design\\cylib\\cylib.{suffix or "cpp"}',
         ],
         include_dirs=[
-            opencv_home + '/include',
+            opencv_include_dir,
             'src\\graduate_design\\cylib\\cpp\\include'
         ],
-        library_dirs=[
-            opencv_home + '/x64/mingw/lib'
-        ],
-        libraries=[
-            'opencv_world470',
-        ]
+        library_dirs=library_dirs,
+        libraries=libraries
     ),
 ]
 if suffix:
+    from Cython.Build import cythonize
     exts = cythonize(exts)
 setup(
     ext_modules=exts,
     zip_safe=False,
-    package_dir={'graduate_design': 'src/graduate_design'}
+    package_dir={'graduate_design': 'src/graduate_design'},
+    cmdclass={'build_ext': Build}
 )
