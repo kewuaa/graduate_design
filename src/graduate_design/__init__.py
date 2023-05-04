e46df28371a2bbf908d80db4591a3b45395f5020
kewuaa <1692525710@qq.com>
2023-05-04 11:35:07 +0800

    add sart

--- src/graduate_design/__init__.py
+++ src/graduate_design/__init__.py
@@ -2,7 +2,7 @@ import cv2
 import torch
 import numpy as np
 from matplotlib import pyplot as plt
-from skimage.transform import iradon
+from skimage.transform import iradon, iradon_sart
 from skimage.metrics import (
     mean_squared_error as MSE,
     peak_signal_noise_ratio as PSNR,
@@ -14,6 +14,7 @@ from . import config
 from .data import dataset
 from .utils.tools import timer
 iradon = timer(iradon)
+iradon_sart = timer(iradon_sart)
 config = config.get('data')
 generate_data = dataset.init
 net = None
@@ -42,31 +43,35 @@ def train(*, device: str = None):
 def validate(checkpoint_index: int = None, device: str = None):
     net.auto_load(checkpoint_index, device)
     img, label, pre = net.validate()
+    img = img / 255
+    label = label / 255
+    pre = pre / 255
     thetas = np.arange(0., 180., step=config.theta_step)
     iradon_img = iradon(img, theta=thetas, filter_name='ramp')
-    iradon_img_ = iradon(img, theta=thetas, filter_name='shepp-logan')
-    iradon_img = np.uint8(
-        (iradon_img - iradon_img.min()) / iradon_img.ptp() * 255
-    )
-    iradon_img_ = np.uint8(
-        (iradon_img_ - iradon_img_.min()) / iradon_img_.ptp() * 255
-    )
+    sart_img = iradon_sart(img, theta=thetas)
+    # iradon_img_ = iradon(img, theta=thetas, filter_name='shepp-logan')
+    iradon_img = (iradon_img - iradon_img.min()) / iradon_img.ptp()
+    sart_img = (sart_img - sart_img.min()) / sart_img.ptp()
+    # iradon_img_ = (iradon_img_ - iradon_img_.min()) / iradon_img_.ptp()
     mse_net = MSE(label, pre)
     psnr_net = PSNR(label, pre)
     ssim_net = SSIM(label, pre)
     mse_fbp = MSE(label, iradon_img)
     psnr_fbp = PSNR(label, iradon_img)
     ssim_fbp = SSIM(label, iradon_img)
-    mse_fbp_ = MSE(label, iradon_img_)
-    psnr_fbp_ = PSNR(label, iradon_img_)
-    ssim_fbp_ = SSIM(label, iradon_img_)
+    mse_sart = MSE(label, sart_img)
+    psnr_sart = PSNR(label, sart_img)
+    ssim_sart = SSIM(label, sart_img)
+    # mse_fbp_ = MSE(label, iradon_img_)
+    # psnr_fbp_ = PSNR(label, iradon_img_)
+    # ssim_fbp_ = SSIM(label, iradon_img_)
     print(f'''
-    MSE of FBP with ramp filter is {mse_fbp}
-    PSNR of FBP with ramp filter is {psnr_fbp}
-    SSIM of FBP with ramp filter is {ssim_fbp}
-    MSE of FBP with shepp-logan filter is {mse_fbp_}
-    PSNR of FBP with shepp-logan filter is {psnr_fbp_}
-    SSIM of FBP with shepp-logan filter is {ssim_fbp_}
+    MSE of FBP is {mse_fbp}
+    PSNR of FBP is {psnr_fbp}
+    SSIM of FBP is {ssim_fbp}
+    MSE of sart is {mse_sart}
+    PSNR of sart is {psnr_sart}
+    SSIM of sart is {ssim_sart}
     MSE of net is {mse_net}
     PSNR of net is {psnr_net}
     SSIM of net is {ssim_net}
@@ -74,15 +79,17 @@ def validate(checkpoint_index: int = None, device: str = None):
     plt.subplot(151)
     plt.title('sinogram')
     plt.imshow(img, cmap='gray')
+    axis = plt.gca()
+    axis.set_aspect(1. / axis.get_data_ratio())
     plt.subplot(152)
-    plt.title('real image')
+    plt.title('Ground truth')
     plt.imshow(label, cmap='gray')
     plt.subplot(153)
-    plt.title('FBP with ramp filter')
+    plt.title('FBP')
     plt.imshow(iradon_img, cmap='gray')
     plt.subplot(154)
-    plt.title('FBP with shepp-logan filter')
-    plt.imshow(iradon_img_, cmap='gray')
+    plt.title('sart')
+    plt.imshow(sart_img, cmap='gray')
     plt.subplot(155)
     plt.title('predicted image')
     plt.imshow(pre, cmap='gray')
@@ -95,16 +102,18 @@ def predict(img_path: str, checkpoint_index: int = None, device: str = None):
     pre = net.predict(img)
     thetas = np.arange(0., 180., step=config.theta_step)
     iradon_img = iradon(img, theta=thetas, filter_name='ramp')
-    iradon_img_ = iradon(img, theta=thetas, filter_name='shepp-logan')
+    sart_img = iradon_sart(img, theta=thetas)
     plt.subplot(141)
     plt.title('sinogram')
     plt.imshow(img, cmap='gray')
+    axis = plt.gca()
+    axis.set_aspect(1. / axis.get_data_ratio())
     plt.subplot(142)
-    plt.title('FBP with ramp filter')
+    plt.title('FBP')
     plt.imshow(iradon_img, cmap='gray')
     plt.subplot(143)
-    plt.title('FBP with shepp-logan filter')
-    plt.imshow(iradon_img_, cmap='gray')
+    plt.title('sart')
+    plt.imshow(sart_img, cmap='gray')
     plt.subplot(144)
     plt.title('predicted image')
     plt.imshow(pre, cmap='gray')
